a1e0a3617d030bbe086f9bd1c13702f25f0eace4
Jim Whiteman <jimtron9000@gmail.com>
2021-04-18 19:01:01 -0700

    Incremental improvement

--- hamming.lisp
+++ hamming.lisp
@@ -3,29 +3,28 @@
 (defun unpack (str)
   (map 'list #'char-code str))
 
-(defun different-right-bit? (byte1 byte2)
-  (/= (logand byte1 1) (logand byte2 1)))
-
 (defun byte-distance (byte1 byte2)
-  (labels ((_ (b1 b2 i acc)
-	     (if (zerop i)
-		 acc
-		 (_ (ash b1 -1)
-		    (ash b2 -1)
-		    (1- i)
-		    (if (different-right-bit? b1 b2)
-			(1+ acc)
-			acc)))))
-    (_ byte1 byte2 8 0)))
+  "Iterate through the xor of the two bytes, shifting each
+  time. If the result is odd, then we can count it as a
+  differing bit"
+  (labels ((_ (xor i acc)
+              (if (zerop i)
+                acc
+                (_ (ash xor -1)
+                   (1- i)
+                   (if (= 1 (logand xor 1))
+                     (1+ acc)
+                     acc)))))
+    (_ (logxor byte1 byte2) 8 0)))
 
 (defun distance (str1 str2)
   "Get the hamming distance between two strings by counting
-   the number of different bits in equal length strings."
+  the number of different bits in equal length strings."
   (labels ((_ (bytes1 bytes2 acc)
-	     (if (null bytes1)
-		 acc
-		 (_ (cdr bytes1)
-		    (cdr bytes2)
-		    (+ acc (byte-distance (car bytes1)
-					  (car bytes2)))))))
+              (if (null bytes1)
+                acc
+                (_ (cdr bytes1)
+                   (cdr bytes2)
+                   (+ acc (byte-distance (car bytes1)
+                                         (car bytes2)))))))
     (_ (unpack str1) (unpack str2) 0)))
